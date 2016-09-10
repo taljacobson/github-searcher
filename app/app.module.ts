ced94cf7343ae49f597f2b2c3a582508577680c9
taljacobson <tal1989@gmail.com>
2016-09-10 20:51:45 +0300

    update to rc6

---
+++ app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from "@angular/http";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent }  from './approot/app.component';
+import { ProfileComponent } from "./components/profile/profile.component";
+
+import { GithubService } from "./services/github.service";
+
+@NgModule({
+  imports: [ BrowserModule, HttpModule, FormsModule ],
+  declarations: [ AppComponent, ProfileComponent ],
+  providers: [ GithubService ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
